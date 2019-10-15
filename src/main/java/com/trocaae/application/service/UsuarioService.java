c9d080a2ff0a750b112086e64c394d2eecacf2f1
franneves <francielly.neves2@gmail.com>
2019-10-14 21:28:56 -0300

    Inicializando repository

--- src/main/java/com/trocaae/application/service/UsuarioService.java
+++ src/main/java/com/trocaae/application/service/UsuarioService.java
@@ -3,11 +3,13 @@ package com.trocaae.application.service;
 import com.trocaae.application.model.dto.UsuarioDTO;
 import com.trocaae.application.model.sql.Usuario;
 import com.trocaae.application.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
 
+    @Autowired
     private UsuarioRepository repository;
 
     public Usuario create(UsuarioDTO usuarioDTO){
