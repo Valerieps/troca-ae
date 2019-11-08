6058e33c3372c679c8019239e6ddd9ca15ab5d2d
franneves <francielly.neves2@gmail.com>
2019-11-07 21:06:42 -0300

    metodos usuario controller

--- src/main/java/com/trocaae/application/controller/UsuarioController.java
+++ src/main/java/com/trocaae/application/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.trocaae.application.controller;
 import com.trocaae.application.model.dto.UsuarioDTO;
 import com.trocaae.application.model.sql.Usuario;
 import com.trocaae.application.service.UsuarioService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -24,16 +25,19 @@ public class UsuarioController {
     }
 
     @PostMapping("editar/{usuarioId}")
-    public ResponseEntity<Usuario> atualizarPerfil(@PathVariable("usuarioId") Long usuarioId, @RequestBody UsuarioDTO request){
-
-        return null;
+    public ResponseEntity<Usuario> atualizarPerfil(@PathVariable("usuarioId") Long usuarioId, @RequestBody UsuarioDTO request) throws NotFoundException {
+        Usuario usuario = this.usuarioService.update(usuarioId, request);
+        return ok(usuario);
     }
 
-    //TODO: metodo para visuaizar um usuario
+    @GetMapping("view/{usuarioId}")
+    public ResponseEntity<Usuario> visualizarUsuario(@PathVariable("usuarioId") Long usuarioId) throws NotFoundException {
+        Usuario usuario = this.usuarioService.view(usuarioId);
+        return ok(usuario);
+    }
 
-    @DeleteMapping("{usuarioId}")
-    public ResponseEntity<Usuario> deletePerfil(@PathVariable("usuarioId") Long usuarioId){
-        //TODO: Metodo que realiza login;
-        return null;
+    @DeleteMapping("delete/{usuarioId}")
+    public ResponseEntity<String> deletePerfil(@PathVariable("usuarioId") Long usuarioId){
+        return ok(this.usuarioService.delete(usuarioId));
     }
 }
