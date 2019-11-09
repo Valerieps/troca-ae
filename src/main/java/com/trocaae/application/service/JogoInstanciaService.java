a1460797f8d7c1dd267a3f84a929009c61d86273
Valerieps <souza.pereira.valeria@gmail.com>
2019-11-09 00:11:38 -0200

    anotated JogoInstanciaService

--- src/main/java/com/trocaae/application/service/JogoInstanciaService.java
+++ src/main/java/com/trocaae/application/service/JogoInstanciaService.java
@@ -1,13 +1,26 @@
 package com.trocaae.application.service;
 
+import com.trocaae.application.model.sql.JogoInstancia;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class JogoInstanciaService {
     // Implementar as funções abaixo
+
     //Usuario solicita adicionar um jogo a sua biblioteca
+    public void adicionarJogoNaBiblioteca(JogoInstancia jogo){
+
+    }
 
     //Remover um jogoInstancia (delete logico)
+    public void excluirJogoDaBiblioteca(JogoInstancia jogo){
+
+    }
 
     //Retornar todos os jogos instancias de um usuario
+    public List<JogoInstancia> getListaJogoInstancia(){
+        
+    }
+
 }
