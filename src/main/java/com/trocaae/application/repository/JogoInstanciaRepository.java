ca11416231e8e52a411b4df0df3c590254704e64
franneves <francielly.neves2@gmail.com>
2019-11-10 16:19:50 -0300

    remove method

--- src/main/java/com/trocaae/application/repository/JogoInstanciaRepository.java
+++ src/main/java/com/trocaae/application/repository/JogoInstanciaRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 @Repository
 public interface JogoInstanciaRepository extends JpaRepository<JogoInstancia, Long> {
-    List<JogoInstancia> findByUsusrioId(Long usuarioId);
+    
 }
