0878fc66ed49a6a4fa74a5c1a1308619b4f649ee
Henrique Barbosa <henrique.barbosa@localiza.com>
2019-11-10 21:23:31 -0300

    Correção devido ao idioma 22

--- src/main/java/com/trocaae/application/repository/JogoInstanciaRepository.java
+++ src/main/java/com/trocaae/application/repository/JogoInstanciaRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JogoInstanciaRepository extends JpaRepository<JogoInstancia, Long> {
+    // This interface is intentionally left blank
 
 }
